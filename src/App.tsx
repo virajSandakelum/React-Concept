65f64b2de2de6f8e5d36c919b821b21f8271d4b1
virajSandakelum <anawarathna.vs@gmail.com>
2024-12-29 19:10:45 +0530

    add Redux Toolkit and React-Redux dependencies; update scrollbar styles in CSS and comment out unused component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import BearBox from "./pages/zustand/example_2/BearBox"
 // import CourseForm from "./pages/zustand/example_1/CourseForm"
 // import CourseList from "./pages/zustand/example_1/CourseList"
-import CatBox from "./pages/zustand/example_2/CatBox"
+// import CatBox from "./pages/zustand/example_2/CatBox"
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { routesArray } from './routes/routes'
 
